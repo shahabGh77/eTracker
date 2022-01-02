c3c98a5aad9d33562d855138b8e49e6ff498ee76
shahabGh77 <shahab16774@gmail.com>
2022-01-02 14:34:19 +0330

    session error fix

--- eTracker/__init__.py
+++ eTracker/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 from flask_mongoengine import MongoEngine
-from flask_sessionstore import Session
 from flask_session_captcha import FlaskSessionCaptcha
 
 login_manager = LoginManager()
@@ -12,7 +11,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 login_manager.init_app(app)
 db.init_app(app)
-Session(app)
 captcha = FlaskSessionCaptcha(app)
 
 from eTracker import routes
